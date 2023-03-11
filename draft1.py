a7e238d2768e6b98ede412c1f55fdd7d09d51a74
ktn <ruqsuqtuq24@gmail.com>
2023-03-11 23:23:23 +0900

    append and remove input table. if rect exist, can't remove input table

--- draft1.py
+++ draft1.py
@@ -10,47 +10,47 @@ time_from=2.5
 time_to=4
 
 class Application(ttk.Frame):   #メインフレーム Frameクラスを継承  
+    InputTableList = [] #InputTableListを初期化
     class InputTable:
         InputTableNum = 0
-        InputTableList = []
-        class RectGeneration:
-            def __init__(self):                  # コンストラクタ
-                self.canv = None
-                self.rect_exist=False
-            def DrawRect(self, parent_frame, ent_from_time, ent_from_minute, ent_to_time, ent_to_minute):
-                def inner():
-                    if self.canv==None:
-                        if str.isdigit(ent_from_time.get()) and str.isdigit(ent_from_minute.get()) and str.isdigit(ent_to_time.get()) and str.isdigit(ent_to_minute.get()):
-                            #開始時刻[分]
-                            start_minute = int(ent_from_time.get())*60 + int(ent_from_minute.get())
-                            #終了時刻[分]
-                            end_minute = int(ent_to_time.get())*60 + int(ent_to_minute.get())
-                            #矩形width計算
-                            rect_width=(end_minute - start_minute)*40/60
-                            print(rect_width)
-                            rect_return=Canvas(parent_frame, relief=FLAT,bg='gold',width=rect_width,height=20,highlightthickness=0)
-                            rect_return.place(x=start_minute*40/60,y=0) 
-                            self.canv = rect_return
-                            self.rect_exist=True
-                            print(self.canv)
-                        else:
-                            print("Enter Digits!")
-                    else:
-                        pass
-                return inner
-            def RmRect(self):
-                def inner():
-                    if self.canv!=None:
-                        self.canv.destroy()
-                        self.canv=None
-                        self.rect_exist=False
+           
+        def DrawRect(self, parent_frame, ent_from_time, ent_from_minute, ent_to_time, ent_to_minute):
+            def inner():
+                if self.canv==None:
+                    if str.isdigit(ent_from_time.get()) and str.isdigit(ent_from_minute.get()) and str.isdigit(ent_to_time.get()) and str.isdigit(ent_to_minute.get()):
+                        #開始時刻[分]
+                        start_minute = int(ent_from_time.get())*60 + int(ent_from_minute.get())
+                        #終了時刻[分]
+                        end_minute = int(ent_to_time.get())*60 + int(ent_to_minute.get())
+                        #矩形width計算
+                        rect_width=(end_minute - start_minute)*40/60
+                        print(rect_width)
+                        rect_return=Canvas(parent_frame, relief=FLAT,bg='gold',width=rect_width,height=20,highlightthickness=0)
+                        rect_return.place(x=start_minute*40/60,y=0) 
+                        self.canv = rect_return
+                        self.rect_exist=True
                         print(self.canv)
                     else:
-                        print(self.canv)
-                return inner        
+                        print("Enter Digits!")
+                else:
+                    pass
+            return inner
+        def RmRect(self):
+            def inner():
+                if self.canv!=None:
+                    self.canv.destroy()
+                    self.canv=None
+                    self.rect_exist=False
+                    print(self.canv)
+                else:
+                    print(self.canv)
+            return inner        
 
         def __init__(self,ParentFrame,frame_TimeTable_Table):                  # コンストラクタ
-            
+            self.canv = None
+            self.rect_exist=False
+            self.parent_frame = ParentFrame
+        
             #入力部
             
             label_title=Label(ParentFrame,text="タイトル",bg=COLOR_OF_TIME_AXIS,font=("",'10'))
@@ -89,39 +89,20 @@ class Application(ttk.Frame):   #メインフレーム Frameクラスを継承
             label_TimeInput_minute=Label(ParentFrame,text="分",bg=COLOR_OF_TIME_AXIS,font=("",'10'))
             label_TimeInput_minute.grid(row=0,column=11)
             
-            rect_timetable=self.RectGeneration()
-            button_Time = Button(ParentFrame, text="Enter",command=rect_timetable.DrawRect(frame_TimeTable_Table, 
+            button_Time = Button(ParentFrame, text="Enter",command=self.DrawRect(frame_TimeTable_Table, 
                                                                                     ent_TimeInput_from_time,
                                                                                     ent_TimeInput_from_minute,
                                                                                     ent_TimeInput_to_time,
                                                                                     ent_TimeInput_to_minute,
                                                                                     ))
             button_Time.grid(row=0,column=12)
-            button_remove = Button(ParentFrame, text="Remove",command=rect_timetable.RmRect())
+            button_remove = Button(ParentFrame, text="Remove",command=self.RmRect())
             #button_remove = Button(ParentFrame, text="Enter",command=self.RemoveWidget_place(rect))
             button_remove.grid(row=0,column=13)
     
-    def ApendInputTable(self, ParentFrame, frame_TimeTable_Table,frame_button_TimeInput):
-        def inner():                
-            self.InputTable.InputTableList.append(Frame(ParentFrame, relief=FLAT,bg='gray',width=1000,height=100))
-            self.InputTable.InputTableList[-1].grid(row=self.InputTable.InputTableNum+1,column=0, columnspan=2)
-            self.InputTable(self.InputTable.InputTableList[-1],frame_TimeTable_Table)
-            frame_button_TimeInput.grid_forget()   
-            frame_button_TimeInput.grid(row=self.InputTable.InputTableNum + 2,column=0,sticky=N)
-            self.InputTable.InputTableNum = self.InputTable.InputTableNum + 1
-        return inner
-    
-    def RmInputTable(self):
-        def inner(): 
-            if self.InputTable.RectGeneration.rect_exist:
-                print("exist rect")
-            else:
-                self.InputTable.InputTableList[-1].grid_forget()
-                self.InputTable.InputTableList.pop()
-        return inner
-
     def __init__(self, master = None):  #コンストラクタ(インスタンス生成時に実行される処理)
         super().__init__(master) #Frame(継承元)のコンストラクタを実行.親ウィジェットを設定
+        
         #ウインドウの設定
         self.master.title("旅行プラン") # ウィンドウタイトル
         self.master.geometry("1200x400")     # ウィンドウサイズ(幅x高さ)
@@ -172,6 +153,27 @@ class Application(ttk.Frame):   #メインフレーム Frameクラスを継承
         button_TimeInput_Rm = Button(frame_button_TimeInput)
         button_TimeInput_Rm.configure(text="削除", command=self.RmInputTable())
         button_TimeInput_Rm.grid(row=0,column=1)
+
+    def ApendInputTable(self, ParentFrame, frame_TimeTable_Table,frame_button_TimeInput):
+        def inner():
+            frame=Frame(ParentFrame, relief=FLAT,bg='gray',width=1000,height=100)
+            frame.grid(row=self.InputTable.InputTableNum + 1,column=0)
+            self.InputTableList.append(self.InputTable(frame, frame_TimeTable_Table))
+            frame_button_TimeInput.grid_forget()   
+            frame_button_TimeInput.grid(row=self.InputTable.InputTableNum + 2,column=0,sticky=N)
+            self.InputTable.InputTableNum = self.InputTable.InputTableNum + 1
+        return inner
+    
+    def RmInputTable(self):
+        def inner(): 
+            if self.InputTableList[-1].rect_exist:
+                print("exist rect")
+            else:
+                self.InputTableList[-1].parent_frame.grid_forget()
+                self.InputTableList.pop()
+        return inner
+
+    
 
 
 if __name__ == "__main__":
